7f8cbd3a389ca4fada0649b3ed4844fd486458e6
gggrdinosaur <1463235793@qq.com>
2022-05-12 22:11:21 +0800

    修改

--- flutter_application_2/flutter_application_1/lib/main.dart
+++ flutter_application_2/flutter_application_1/lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title:'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       home: const GetStateObjectRoute(),
     );

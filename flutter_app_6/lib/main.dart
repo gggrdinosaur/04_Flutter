036792d1bdcf8480bf32eebf80e6d098331a7fb3
gggrdinosaur <1463235793@qq.com>
2022-06-09 15:48:54 +0800

    新增

--- flutter_app_6/lib/main.dart
+++ flutter_app_6/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const TabViewRoute1( ),
+      home:  const MyHomePage(title: 'APP ',),
     );
   }
 }
@@ -30,7 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -43,6 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(icon: const Icon(Icons.share), onPressed: () {}),
         ],
       ),
+      //body: const SingleChildScrollViewTestRoute(),  //SingleChildScrollViewTestRoute组件
+      //body:const ListView2(),  //ListView组件
+       //body:AnimatedListRoute(),  //AnimatedListRoute组件
+       body:InfiniteGridView(),   //InfiniteGridView组件
       drawer: const MyDrawer(), //抽屉
       bottomNavigationBar: BottomAppBar(
         //底部导航栏
@@ -54,20 +57,17 @@ class _MyHomePageState extends State<MyHomePage> {
             IconButton(
                 icon: const Icon(Icons.home),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
             IconButton(
                 icon: const Icon(Icons.business),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
             IconButton(
                 icon: const Icon(Icons.schedule),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
           ],
           mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
+//TabViewRoute组件
 class TabViewRoute1 extends StatefulWidget {
   const TabViewRoute1({Key? key}) : super(key: key);
 
@@ -87,7 +87,7 @@ class TabViewRoute1 extends StatefulWidget {
 class _TabViewRoute1State extends State<TabViewRoute1>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  List tabs = ["新闻", "历史", "图片"];
+  List tabs = ["新闻", "历史", "图片", "浏览"];
 
   @override
   void initState() {
@@ -117,8 +117,8 @@ class _TabViewRoute1State extends State<TabViewRoute1>
           return KeepAliveWrapper(
             child: Container(
               alignment: Alignment.center,
-              child: Text(e, textScaleFactor: 5),
-            ),
+              child: const ListView2(),
+              ),
           );
         }).toList(),
       ),
@@ -133,20 +133,17 @@ class _TabViewRoute1State extends State<TabViewRoute1>
             IconButton(
                 icon: const Icon(Icons.home),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
             IconButton(
                 icon: const Icon(Icons.business),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
             IconButton(
                 icon: const Icon(Icons.schedule),
                 onPressed: () {
-                  setState(() {
-                  });
+                  setState(() {});
                 }),
           ],
           mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
@@ -163,6 +160,71 @@ class _TabViewRoute1State extends State<TabViewRoute1>
   }
 }
 
+class TabViewRoute2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List tabs = ["新闻", "历史", "图片","浏览"];
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("App Name"),
+          bottom: TabBar(
+            tabs: tabs.map((e) => Tab(text: e)).toList(),
+          ),
+        ),
+        body: TabBarView( //构建
+          children: tabs.map((e) {
+            return KeepAliveWrapper(
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(e, textScaleFactor: 5),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+
+class KeepAliveWrapper extends StatefulWidget {
+  const KeepAliveWrapper({
+    Key? key,
+    this.keepAlive = true,
+    required this.child,
+  }) : super(key: key);
+  final bool keepAlive;
+  final Widget child;
+
+  @override
+  _KeepAliveWrapperState createState() => _KeepAliveWrapperState();
+}
+
+class _KeepAliveWrapperState extends State<KeepAliveWrapper>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return widget.child;
+  }
+
+  @override
+  void didUpdateWidget(covariant KeepAliveWrapper oldWidget) {
+    if(oldWidget.keepAlive != widget.keepAlive) {
+      // keepAlive 状态需要更新，实现在 AutomaticKeepAliveClientMixin 中
+      updateKeepAlive();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  bool get wantKeepAlive => widget.keepAlive;
+}
+
+
+//抽屉
 class MyDrawer extends StatelessWidget {
   const MyDrawer({
     Key? key,
@@ -218,7 +280,7 @@ class MyDrawer extends StatelessWidget {
     );
   }
 }
-
+//singleChildScrollViewTestRoute组件
 class SingleChildScrollViewTestRoute extends StatelessWidget {
   const SingleChildScrollViewTestRoute({Key? key}) : super(key: key);
 
@@ -254,8 +316,10 @@ class SingleChildScrollViewTestRoute extends StatelessWidget {
     );
   }
 }
-
+//ListView组件
 class ListView2 extends StatelessWidget {
+  const ListView2({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     //下划线widget预定义以供复用。
@@ -268,58 +332,164 @@ class ListView2 extends StatelessWidget {
   }
 }
 
-class ListView3 extends StatelessWidget {
+
+//AnimatedListRoute组件
+class AnimatedListRoute extends StatefulWidget {
+  const AnimatedListRoute({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    //下划线widget预定义以供复用。
-    Widget divider1 = Divider(
-      color: Colors.blue,
-    );
-    Widget divider2 = Divider(color: Colors.green);
-    return ListView.separated(
-      itemCount: 100,
-      //列表项构造器
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(title: Text("$index"));
-      },
-      //分割器构造器
-      separatorBuilder: (BuildContext context, int index) {
-        return index % 2 == 0 ? divider1 : divider2;
-      },
-    );
-  }
+  _AnimatedListRouteState createState() => _AnimatedListRouteState();
 }
 
-class KeepAliveWrapper extends StatefulWidget {
-  const KeepAliveWrapper({
-    Key? key,
-    this.keepAlive = true,
-    required this.child,
-  }) : super(key: key);
-  final bool keepAlive;
-  final Widget child;
+class _AnimatedListRouteState extends State<AnimatedListRoute> {
+  var data = <String>[];
+  int counter = 5;
+
+  final globalKey = GlobalKey<AnimatedListState>();
 
   @override
-  _KeepAliveWrapperState createState() => _KeepAliveWrapperState();
-}
-
-class _KeepAliveWrapperState extends State<KeepAliveWrapper>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return widget.child;
-  }
-
-  @override
-  void didUpdateWidget(covariant KeepAliveWrapper oldWidget) {
-    if(oldWidget.keepAlive != widget.keepAlive) {
-      // keepAlive 状态需要更新，实现在 AutomaticKeepAliveClientMixin 中
-      updateKeepAlive();
+  void initState() {
+    for (var i = 0; i < counter; i++) {
+      data.add('${i + 1}');
     }
-    super.didUpdateWidget(oldWidget);
+    super.initState();
   }
 
   @override
-  bool get wantKeepAlive => widget.keepAlive;
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AnimatedList(
+          key: globalKey,
+          initialItemCount: data.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+            Animation<double> animation,
+          ) {
+            //添加列表项时会执行渐显动画
+            return FadeTransition(
+              opacity: animation,
+              child: buildItem(context, index),
+            );
+          },
+        ),
+        buildAddBtn(),
+      ],
+    );
+  }
+
+  // 创建一个 “+” 按钮，点击后会向列表中插入一项
+  Widget buildAddBtn() {
+    return Positioned(
+      child: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // 添加一个列表项
+          data.add('${++counter}');
+          // 告诉列表项有新添加的列表项
+          globalKey.currentState!.insertItem(data.length - 1);
+          print('添加 $counter');
+        },
+      ),
+      bottom: 30,
+      left: 0,
+      right: 0,
+    );
+  }
+
+  // 构建列表项
+  Widget buildItem(context, index) {
+    String char = data[index];
+    return ListTile(
+      //数字不会重复，所以作为Key
+      key: ValueKey(char),
+      title: Text(char),
+      trailing: IconButton(
+        icon: Icon(Icons.delete),
+        // 点击时删除
+        onPressed: () => onDelete(context, index),
+      ),
+    );
+  }
+
+  void onDelete(context, index) {
+    setState(() {
+  globalKey.currentState!.removeItem(
+    index,
+    (context, animation) {
+      // 删除过程执行的是反向动画，animation.value 会从1变为0
+      var item = buildItem(context, index);
+      print('删除 ${data[index]}');
+      data.removeAt(index);
+      // 删除动画是一个合成动画：渐隐 + 缩小列表项告诉
+      return FadeTransition(
+        opacity: CurvedAnimation(
+          parent: animation,
+          //让透明度变化的更快一些
+          curve: const Interval(0.5, 1.0),
+        ),
+        // 不断缩小列表项的高度
+        child: SizeTransition(
+          sizeFactor: animation,
+          axisAlignment: 0.0,
+          child: item,
+        ),
+      );
+    },
+    duration: Duration(milliseconds: 200), // 动画时间为 200 ms
+  );
+});
+  }
+}
+
+//GridView组件
+class InfiniteGridView extends StatefulWidget {
+  @override
+  _InfiniteGridViewState createState() => _InfiniteGridViewState();
+}
+
+class _InfiniteGridViewState extends State<InfiniteGridView> {
+  List<IconData> _icons = []; //保存Icon数据
+
+  @override
+  void initState() {
+    super.initState();
+    // 初始化数据
+    _retrieveIcons();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, //每行三列
+        childAspectRatio: 1.0, //显示区域宽高相等
+      ),
+      itemCount: _icons.length,
+      itemBuilder: (context, index) {
+        //如果显示到最后一个并且Icon总数小于200时继续获取数据
+        if (index == _icons.length - 1 && _icons.length < 200) {
+          _retrieveIcons();
+        }
+        return Icon(_icons[index]);
+      },
+    );
+  }
+
+  //模拟异步获取数据
+  void _retrieveIcons() {
+    Future.delayed(Duration(milliseconds: 200)).then((e) {
+      setState(() {
+        _icons.addAll([
+          Icons.ac_unit,
+          Icons.airport_shuttle,
+          Icons.all_inclusive,
+          Icons.beach_access,
+          Icons.cake,
+          Icons.free_breakfast,
+        ]);
+      });
+    });
+  }
 }

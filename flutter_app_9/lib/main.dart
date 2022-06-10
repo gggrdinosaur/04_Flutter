e979d300a828246a0a50bc30d847de6b6ae7e5d8
gggrdinosaur <1463235793@qq.com>
2022-06-10 23:24:48 +0800

    第十章

---
+++ flutter_app_9/lib/main.dart
@@ -0,0 +1,715 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import '../widgets/index.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+        primaryColor: Colors.green,
+      ),
+      home: const MyHomePage(title: 'APP',),
+      //home:AnimatedWidgetsTest(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  double _turns = .0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(
+        children: <Widget>[
+          GradientButton(
+            colors: const [Colors.greenAccent, Colors.green],
+            height: 50.0,
+            child: const Text("Submit"),
+            onPressed: onTap,
+          ),
+          TurnBox(
+            turns: _turns,
+            speed: 1000,
+            child: const Icon(
+              Icons.refresh,
+              size: 50.0,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text("顺时针旋转1/5圈"),
+            onPressed: () {
+              setState(() {
+                _turns += .2;
+              });
+            },
+          ),
+          ElevatedButton(
+            child: const Text("逆时针旋转1/5圈"),
+            onPressed: () {
+              setState(() {
+                _turns -= .2;
+              });
+            },
+          ),
+          //CustomPaintRoute(),    //棋子
+
+           HeroAnimationRouteA(), //hero动画
+          StaggerRoute(), //交织动画
+          AnimatedSwitcherCounterRoute(), //动画切换
+          ScaleAnimationRoute(), //缩放动画
+          ScaleAnimationRoute1(), //循环缩放动画
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  onTap() {
+    print("button click");
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    print('paint');
+    var rect = Offset.zero & size;
+    //画棋盘
+    drawChessboard(canvas, rect);
+    //画棋子
+    drawPieces(canvas, rect);
+  }
+
+  // 返回false, 后面介绍
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+void drawChessboard(Canvas canvas, Rect rect) {
+  //棋盘背景
+  var paint = Paint()
+    ..isAntiAlias = true
+    ..style = PaintingStyle.fill //填充
+    ..color = Color(0xFFDCC48C);
+  canvas.drawRect(rect, paint);
+
+  //画棋盘网格
+  paint
+    ..style = PaintingStyle.stroke //线
+    ..color = Colors.black38
+    ..strokeWidth = 1.0;
+
+  //画横线
+  for (int i = 0; i <= 15; ++i) {
+    double dy = rect.top + rect.height / 15 * i;
+    canvas.drawLine(Offset(rect.left, dy), Offset(rect.right, dy), paint);
+  }
+
+  for (int i = 0; i <= 15; ++i) {
+    double dx = rect.left + rect.width / 15 * i;
+    canvas.drawLine(Offset(dx, rect.top), Offset(dx, rect.bottom), paint);
+  }
+}
+
+//画棋子
+void drawPieces(Canvas canvas, Rect rect) {
+  double eWidth = rect.width / 15;
+  double eHeight = rect.height / 15;
+  //画一个黑子
+  var paint = Paint()
+    ..style = PaintingStyle.fill
+    ..color = Colors.black;
+  //画一个黑子
+  canvas.drawCircle(
+    Offset(rect.center.dx - eWidth / 2, rect.center.dy - eHeight / 2),
+    min(eWidth / 2, eHeight / 2) - 2,
+    paint,
+  );
+  //画一个白子
+  paint.color = Colors.white;
+  canvas.drawCircle(
+    Offset(rect.center.dx + eWidth / 2, rect.center.dy - eHeight / 2),
+    min(eWidth / 2, eHeight / 2) - 2,
+    paint,
+  );
+}
+
+class CustomPaintRoute extends StatelessWidget {
+  const CustomPaintRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CustomPaint(
+            size: Size(300, 300), //指定画布大小
+            painter: MyPainter(),
+          ),
+          //添加一个刷新button
+          ElevatedButton(onPressed: () {}, child: Text("刷新"))
+        ],
+      ),
+    );
+  }
+}
+
+class ScaleAnimationRoute extends StatefulWidget {
+  const ScaleAnimationRoute({Key? key}) : super(key: key);
+
+  @override
+  _ScaleAnimationRouteState createState() => _ScaleAnimationRouteState();
+}
+
+//需要继承TickerProvider，如果有多个AnimationController，则应该使用TickerProviderStateMixin。
+class _ScaleAnimationRouteState extends State<ScaleAnimationRoute>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    //使用弹性曲线
+    animation = CurvedAnimation(parent: controller, curve: Curves.bounceIn);
+    //图片宽高从0变到300
+    animation = Tween(begin: 0.0, end: 300.0).animate(animation)
+      ..addListener(() {
+        setState(() => {});
+      });
+    //启动动画
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.network(
+        "https://tse4-mm.cn.bing.net/th/id/OIP-C.bRj4FIhvHMrgJPyg_RnBqgHaD6?w=312&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        width: animation.value,
+        height: animation.value,
+      ),
+    );
+  }
+
+  @override
+  dispose() {
+    //路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedImage extends AnimatedWidget {
+  const AnimatedImage({
+    Key? key,
+    required Animation<double> animation,
+  }) : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Image.network(
+        "https://tse4-mm.cn.bing.net/th/id/OIP-C.bRj4FIhvHMrgJPyg_RnBqgHaD6?w=312&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        width: animation.value,
+        height: animation.value,
+      ),
+    );
+  }
+}
+
+class ScaleAnimationRoute1 extends StatefulWidget {
+  const ScaleAnimationRoute1({Key? key}) : super(key: key);
+
+  @override
+  _ScaleAnimationRouteState1 createState() => _ScaleAnimationRouteState1();
+}
+
+class _ScaleAnimationRouteState1 extends State<ScaleAnimationRoute1>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    //图片宽高从0变到300
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        //动画执行结束时反向执行动画
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        //动画恢复到初始状态时执行动画（正向）
+        controller.forward();
+      }
+    });
+
+    //启动动画（正向）
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedImage(
+      animation: animation,
+    );
+  }
+
+  @override
+  dispose() {
+    //路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+//Hero动画
+class HeroAnimationRouteA extends StatelessWidget {
+  const HeroAnimationRouteA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: <Widget>[
+          InkWell(
+            child: Hero(
+              tag: "avatar", //唯一标记，前后两个路由页Hero的tag必须相同
+              child: ClipOval(
+                child: Image.network(
+                  "https://tse4-mm.cn.bing.net/th/id/OIP-C.bRj4FIhvHMrgJPyg_RnBqgHaD6?w=312&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+                  width: 50.0,
+                ),
+              ),
+            ),
+            onTap: () {
+              //打开B路由
+              Navigator.push(context, PageRouteBuilder(
+                pageBuilder: (
+                  BuildContext context,
+                  animation,
+                  secondaryAnimation,
+                ) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: Scaffold(
+                      appBar: AppBar(
+                        title: const Text("原图"),
+                      ),
+                      body: HeroAnimationRouteB(),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Text("点击头像"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HeroAnimationRouteB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Hero(
+        tag: "avatar", //唯一标记，前后两个路由页Hero的tag必须相同
+        child: Image.network(
+            "https://tse4-mm.cn.bing.net/th/id/OIP-C.bRj4FIhvHMrgJPyg_RnBqgHaD6?w=312&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"),
+      ),
+    );
+  }
+}
+
+//交织动画
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation({
+    Key? key,
+    required this.controller,
+  }) : super(key: key) {
+    //高度动画
+    height = Tween<double>(
+      begin: .0,
+      end: 300.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.0, 0.6, //间隔，前60%的动画时间
+          curve: Curves.ease,
+        ),
+      ),
+    );
+
+    color = ColorTween(
+      begin: Colors.green,
+      end: Colors.greenAccent,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.0, 0.6, //间隔，前60%的动画时间
+          curve: Curves.ease,
+        ),
+      ),
+    );
+
+    padding = Tween<EdgeInsets>(
+      begin: const EdgeInsets.only(left: .0),
+      end: const EdgeInsets.only(left: 100.0),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.6, 1.0, //间隔，后40%的动画时间
+          curve: Curves.ease,
+        ),
+      ),
+    );
+  }
+
+  late final Animation<double> controller;
+  late final Animation<double> height;
+  late final Animation<EdgeInsets> padding;
+  late final Animation<Color?> color;
+
+  Widget _buildAnimation(BuildContext context, child) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: padding.value,
+      child: Container(
+        color: color.value,
+        width: 50.0,
+        height: height.value,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
+    );
+  }
+}
+
+class StaggerRoute extends StatefulWidget {
+  @override
+  _StaggerRouteState createState() => _StaggerRouteState();
+}
+
+class _StaggerRouteState extends State<StaggerRoute>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+  }
+
+  _playAnimation() async {
+    try {
+      //先正向执行动画
+      await _controller.forward().orCancel;
+      //再反向执行动画
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () => _playAnimation(),
+            child: Text("start animation"),
+          ),
+          Container(
+            width: 300.0,
+            height: 300.0,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.1),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            //调用我们定义的交错动画Widget
+            child: StaggerAnimation(controller: _controller),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//动画切换
+class AnimatedSwitcherCounterRoute extends StatefulWidget {
+  const AnimatedSwitcherCounterRoute({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedSwitcherCounterRouteState createState() =>
+      _AnimatedSwitcherCounterRouteState();
+}
+
+class _AnimatedSwitcherCounterRouteState
+    extends State<AnimatedSwitcherCounterRoute> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              var tween = Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0));
+              //执行缩放动画
+              return MySlideTransition(
+                child: child,
+                position: tween.animate(animation),
+              );
+            },
+            child: Text(
+              '$_count',
+              //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+              key: ValueKey<int>(_count),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text(
+              '+1',
+            ),
+            onPressed: () {
+              setState(() {
+                _count += 1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MySlideTransition extends AnimatedWidget {
+  const MySlideTransition({
+    Key? key,
+    required Animation<Offset> position,
+    this.transformHitTests = true,
+    required this.child,
+  }) : super(key: key, listenable: position);
+
+  final bool transformHitTests;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final position = listenable as Animation<Offset>;
+    Offset offset = position.value;
+    if (position.status == AnimationStatus.reverse) {
+      offset = Offset(-offset.dx, offset.dy);
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
+    );
+  }
+}
+
+//动画过渡
+class AnimatedWidgetsTest extends StatefulWidget {
+  const AnimatedWidgetsTest({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedWidgetsTestState createState() => _AnimatedWidgetsTestState();
+}
+
+class _AnimatedWidgetsTestState extends State<AnimatedWidgetsTest> {
+  double _padding = 10;
+  var _align = Alignment.topRight;
+  double _height = 100;
+  double _left = 0;
+  Color _color = Colors.red;
+  TextStyle _style = const TextStyle(color: Colors.black);
+  Color _decorationColor = Colors.blue;
+  double _opacity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    var duration = const Duration(milliseconds: 400);
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _padding = 20;
+              });
+            },
+            child: AnimatedPadding(
+              duration: duration,
+              padding: EdgeInsets.all(_padding),
+              child: const Text("AnimatedPadding"),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Stack(
+              children: <Widget>[
+                AnimatedPositioned(
+                  duration: duration,
+                  left: _left,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _left = 100;
+                      });
+                    },
+                    child: const Text("AnimatedPositioned"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            color: Colors.grey,
+            child: AnimatedAlign(
+              duration: duration,
+              alignment: _align,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _align = Alignment.center;
+                  });
+                },
+                child: const Text("AnimatedAlign"),
+              ),
+            ),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            height: _height,
+            color: _color,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  _height = 150;
+                  _color = Colors.blue;
+                });
+              },
+              child: const Text(
+                "AnimatedContainer",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          AnimatedDefaultTextStyle(
+            child: GestureDetector(
+              child: const Text("hello world"),
+              onTap: () {
+                setState(() {
+                  _style = const TextStyle(
+                    color: Colors.blue,
+                    decorationStyle: TextDecorationStyle.solid,
+                    decorationColor: Colors.blue,
+                  );
+                });
+              },
+            ),
+            style: _style,
+            duration: duration,
+          ),
+          AnimatedOpacity(
+            opacity: _opacity,
+            duration: duration,
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              onPressed: () {
+                setState(() {
+                  _opacity = 0.2;
+                });
+              },
+              child: const Text(
+                "AnimatedOpacity",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ].map((e) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: e,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
